61d23af8d86887e92db67531a462e89592233598
Russell Owen <rowen@uw.edu>
2014-01-02 16:49:53 -0800

    Added wrappers for Device, Actor and ActorDispatcher; these are useful for unit tests and for emulating actors with fake devices. DeviceSet.slotListFromBoolList bug fix: it was returning a list of indices, not slot names.

---
+++ python/twistedActor/baseWrapper.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+import sys
+
+from twisted.internet.defer import Deferred
+import RO.AddCallback
+
+__all__ = ["BaseWrapper"]
+
+class BaseWrapper(RO.AddCallback.BaseMixin):
+    """A wrapper for a client talking to a server
+    
+    This wrapper is responsible for starting and stopping everything:
+    - It accepts an actor wrapper
+    - It builds an actor dispatcher when the actor wrapper is ready
+    - It stops both on close()
+    
+    Public attributes include:
+    - actorWrapper: the actor wrapper (twistedActor.ActorWrapper)
+    - dispatcher: the actor dispatcher (twistedActor.ActorDispatcher); None until ready
+    - readyDeferred: called when the dispatcher is ready
+      (for tracking closure use the Deferred returned by the close method, or stateCallback).
+      
+    Subclasses must override:
+    _basicClose
+    isDone
+    isReady,
+    didFail
+    and must call _stateChanged as appropriate; see, e.g. DeviceWrapper
+    """
+    def __init__(self,
+        stateCallback=None,
+        callNow=False,
+    ):
+        """Construct a DispatcherWrapper that manages everything
+
+        @param[in] stateCallback: function to call when connection state of of any socket changes;
+            receives one argument: this actor wrapper
+        @param[in] callNow: call stateCallback now? (Defaults to false because typically
+            subclasses have some additional setup to do before calling callback functions).
+        """
+        RO.AddCallback.BaseMixin.__init__(self, defCallNow=True)
+        self.readyDeferred = Deferred()
+        self._closeDeferred = None
+        self.addCallback(stateCallback, callNow=callNow)
+
+    @property
+    def isReady(self):
+        """Return True if the actor has connected to the fake hardware controller
+        """
+        raise NotImplementedError()
+    
+    @property
+    def isDone(self):
+        """Return True if the actor and fake hardware controller are fully disconnected
+        """
+        raise NotImplementedError()
+    
+    @property
+    def didFail(self):
+        """Return True if isDone and there was a failure
+        """
+        raise NotImplementedError()
+    
+    def _basicClose(self):
+        """Close clients and servers
+        """
+        raise NotImplementedError()
+#         if self.dispatcher:
+#             self.dispatcher.disconnect()
+#         self.actorWrapper.close()
+    
+    def _stateChanged(self, *args):
+        """Called when state changes
+        """
+#         print "%r; _stateChanged()" % (self,)
+        if self._closeDeferred: # closing or closed
+            if self.isDone:
+                if not self.readyDeferred.called:
+                    self.readyDeferred.cancel()
+                if not self._closeDeferred.called:
+                    #print "%s calling closeDeferred" % (self,)
+                    self._closeDeferred.callback(None)
+                else:
+                    sys.stderr.write("Device wrapper state changed after wrapper closed\n")
+        else: # opening or open
+            if not self.readyDeferred.called:
+                if self.isReady:
+                    #print "%s calling readyDeferred" % (self,)
+                    self.readyDeferred.callback(None)
+                elif self.didFail:
+                    #print "%s calling readyDeferred.errback" % (self,)
+                    self.readyDeferred.errback("Failed") # probably should not be a string?
+
+
+        self._doCallbacks()
+        if self.isDone:
+            self._removeAllCallbacks()
+    
+    def close(self):
+        """Close everything
+        
+        @return a deferred
+        """
+        if self._closeDeferred:
+            raise RuntimeError("Already closing or closed")
+
+        self._closeDeferred = Deferred()
+        if not self.readyDeferred.called:
+            self.readyDeferred.cancel()
+        self._basicClose()
+        return self._closeDeferred        
+    
+    def __str__(self):
+        return "%s" % (type(self).__name__,)
+    
+    def __repr__(self):
+        return "%s; isReady=%s, isDone=%s, didFail=%s" % \
+            (type(self).__name__, self.isReady, self.isDone, self.didFail)
---
+++ python/twistedActor/dispatcherWrapper.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+import RO.Comm.Generic
+RO.Comm.Generic.setFramework("twisted")
+from RO.Comm.TCPConnection import TCPConnection
+from .baseWrapper import BaseWrapper
+
+__all__ = ["DispatcherWrapper"]
+
+class DispatcherWrapper(BaseWrapper):
+    """A wrapper for an opscore.ActorDispatcher talking to an wrapped actor
+    
+    This wrapper is responsible for starting and stopping everything:
+    - It builds an actor dispatcher when the actor wrapper is ready
+    - It stops the actor wrapper and actor dispatcher on close()
+    
+    Public attributes include:
+    - actorWrapper: the actor wrapper (twistedActor.ActorWrapper)
+    - dispatcher: the actor dispatcher (twistedActor.ActorDispatcher); None until ready
+    - readyDeferred: called when the dispatcher is ready
+      (for tracking closure use the Deferred returned by the close method, or stateCallback).
+      
+    Subclasses must override _makeDispatcher
+    """
+    def __init__(self,
+        actorWrapper,
+        dictName,
+        readCallback = None,
+        stateCallback = None,
+    ):
+        """Construct a DispatcherWrapper that manages everything
+
+        @param[in] actorWrapper: actor wrapper (twistedActor.ActorWrapper); must be starting up or ready
+        @param[in] dictName: name of actor keyword dictionary
+        @param[in] readCallback: function to call when the actor dispatcher has data to read
+        @param[in] stateCallback: function to call when connection state of of any socket changes;
+            receives one argument: this actor wrapper
+        """
+        BaseWrapper.__init__(self, stateCallback=stateCallback, callNow=False)
+        self.actorWrapper = actorWrapper
+        self._dictName = dictName
+        self._readCallback = readCallback
+        self.dispatcher = None # the ActorDispatcher, once it's built
+        self.actorWrapper.addCallback(self._actorWrapperStateChanged)
+        self._actorWrapperStateChanged()
+    
+    def _makeDispatcher(self, connection):
+        #print "_makeDispatcher()"
+        raise NotImplementedError()
+    
+    @property
+    def actor(self):
+        """Return the actor (in this case, the mirror controller)
+        """
+        return self.actorWrapper.actor
+    
+    @property
+    def userPort(self):
+        """Return the actor port, if known, else None
+        """
+        return self.actorWrapper.userPort
+        
+    @property
+    def isReady(self):
+        """Return True if the actor has connected to the fake hardware controller
+        """
+        return self.actorWrapper.isReady and self.dispatcher and self.dispatcher.connection.isConnected
+    
+    @property
+    def isDone(self):
+        """Return True if the actor and fake hardware controller are fully disconnected
+        """
+        return self.actorWrapper.isDone and self.dispatcher and self.dispatcher.connection.isDisconnected
+    
+    @property
+    def didFail(self):
+        """Return True if isDone and there was a failure
+        """
+        return self.isDone and (self.actorWrapper.didFail or self.dispatcher.connection.didFail)
+    
+    def _actorWrapperStateChanged(self, dumArg=None):
+        """Called when the device wrapper changes state
+        """
+        if self.actorWrapper.isReady and not self.dispatcher:
+            connection = TCPConnection(
+                host = 'localhost',
+                port = self.actorWrapper.userPort,
+                readLines = True,
+                name = "mirrorCtrlConn",
+            )
+            self._makeDispatcher(connection)
+            connection.addStateCallback(self._stateChanged)
+            if self._readCallback:
+                connection.addReadCallback(self._readCallback)
+            connection.connect()
+        self._stateChanged()
+    
+    def _basicClose(self):
+        """Close dispatcher and actor
+        """
+        if self.dispatcher:
+            self.dispatcher.disconnect()
+        self.actorWrapper.close()
