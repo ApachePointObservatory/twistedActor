20a0190dfc1022c1116466760d21db33e11a62cb
Conor Sayres <csayres@uw.edu>
2014-01-14 14:41:04 -0800

    fixed actor example

--- examples/simpleActor.py
+++ examples/simpleActor.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 from twisted.internet import reactor
 from twistedActor import Actor
+class SimpleActor(Actor):
+	## log method must be implemented
+	def logMsg(self, msg):
+		print 'log: %s' % msg
 
 if __name__ == "__main__":
     port = 2005
     
     print "Starting up the actor on port", port
-    Actor(
+    SimpleActor(
         userPort = port,
     )
+
 
     reactor.run()
